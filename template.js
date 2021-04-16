531c5051a639bff6e1782e4c454ace36cad297c4
Edoardo Cavallo <iweek16@gmail.com>
2021-04-16 11:20:03 +0200

    Create template.js

---
+++ template.js
@@ -0,0 +1,59 @@
+'use strict';
+class ClassName extends HTMLElement{
+
+    static get observedAttributes(){
+        return ['title', 'subtitle', 'header-position', 'header-above', 'size', 'width', 'height', 'drag', 'loop', 'progression']
+    }
+
+    static get ATTR(){return new Set(['true', 'false', ''])}
+
+    get attr(){ return this.getAttribute("attr")}
+    set attr(v){this.setAttribute("attr", v)}
+  
+     constructor(){
+        super()
+
+        //#region Root
+        
+        //#endregion
+
+        //#region Fields         
+                    
+        //#endregion
+        
+        
+    }
+
+    connectedCallback(){
+        // this.addEventListener()
+    }
+
+    attributeChangedCallback(name, oldValue, newValue) {
+        switch(name){
+            case 'attr':{
+                if(!ClassName.ATTR.has(newValue)) return console.error('Can be setted only value: ', ClassName.ATTR)
+
+                if(newValue == "true" || newValue == ""){
+                    //
+                }
+                else if(newValue == "false"){
+                    //
+                }
+
+                break;
+            }
+        }
+    }
+
+    //#region Private
+    
+        addEventListener(){}
+
+    //#endregion
+
+    //#region Methods
+    //#endregion
+}
+
+    
+customElements.define('custom-element', ClassName)
